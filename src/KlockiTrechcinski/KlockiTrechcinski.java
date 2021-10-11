287cf5c48fafd100f8b4f8e6c4898f13be341af9
Friste0ne <wtrechc1@stu.vistula.edu.pl>
2021-10-11 14:17:09 +0200

    Initial commit

---
+++ src/KlockiTrechcinski/KlockiTrechcinski.java
@@ -0,0 +1,11 @@
+package KlockiTrechcinski;
+
+public class KlockiTrechcinski {
+    public static void main(String[] args) {
+        int posiadaneKlockiTrechcinski = 75;
+        int pojemnoscKazdegoPojemnika = 6;
+        System.out.println("Wypełnisz "+posiadaneKlockiTrechcinski/pojemnoscKazdegoPojemnika + " pojemniki(ów)");
+        System.out.println("W ostatnim pojemniku zostanie "+posiadaneKlockiTrechcinski%pojemnoscKazdegoPojemnika +
+                " klocki(ów)");
+    }
+}
