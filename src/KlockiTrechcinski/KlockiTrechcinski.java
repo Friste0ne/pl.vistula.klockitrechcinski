f1d359bc65e05f43e31f6e4eac68b8628ee431a0
Friste0ne <wtrechc1@stu.vistula.edu.pl>
2021-10-11 14:24:55 +0200

    lekcja 11.10.2021

--- src/KlockiTrechcinski/KlockiTrechcinski.java
+++ src/KlockiTrechcinski/KlockiTrechcinski.java
@@ -4,7 +4,7 @@ public class KlockiTrechcinski {
     public static void main(String[] args) {
         int posiadaneKlockiTrechcinski = 75;
         int pojemnoscKazdegoPojemnika = 6;
-        System.out.println("Wypełnisz "+posiadaneKlockiTrechcinski/pojemnoscKazdegoPojemnika + " pojemniki(ów)");
+        System.out.println("Wypełnisz "+posiadaneKlockiTrechcinski/pojemnoscKazdegoPojemnika + " pojemniki(ów) po "+pojemnoscKazdegoPojemnika+" klocków");
         System.out.println("W ostatnim pojemniku zostanie "+posiadaneKlockiTrechcinski%pojemnoscKazdegoPojemnika +
                 " klocki(ów)");
     }
